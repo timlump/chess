b16b3cfe4de420deb3faa8b48b4f7255854fbf0d
timlump <costigt@tcd.ie>
2021-12-15 00:10:16 +0000

    got triangle rendering again added cmake

--- main.cpp
+++ main.cpp
@@ -1,124 +1,12 @@
 #include "chess.h"
-#define GLEW_STATIC
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-void error_callback(int error, const char * description)
-{
-    std::cerr << "Error: " << error << std::endl;
-    std::cerr << description << std::endl;
-}
+#include "gfx.h"
 
 int main()
 {
-    GLFWwindow * window = nullptr;
-    if (not glfwInit()) {
-        return -1;
-    }
-    
-    glfwSetErrorCallback(error_callback);
+    gfx::gfx gfx;
 
-    window = glfwCreateWindow(640, 480, "Chess", nullptr, nullptr);
-    if (not window) {
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    // this must be called after getting the opengl context
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Failed to initialize glew\n";
-        return -1;
-    }
-
-    float vertices[] = {
-        0.0f, 0.5f,
-        0.5f,-0.5f,
-       -0.5f,-0.5f
-    };
-
-    GLuint vbo;
-    glGenBuffers(1,&vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    const char* vertex_source = 
-    R"glsl(
-        #version 300 es
-
-        in vec2 position;
-
-        void main()
-        {
-            gl_Position = vec4(position, 0.0, 1.0);
-        }
-    )glsl";
-    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    {
-        glShaderSource(vertex_shader, 1, &vertex_source, nullptr);
-        glCompileShader(vertex_shader);
-        GLint status;
-        glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &status);
-        if (status != GL_TRUE) {
-            char buffer[512];
-            glGetShaderInfoLog(vertex_shader, 512, NULL, buffer);
-            std::cerr << "Vertex shader error: " << buffer << std::endl;
-        }
-    }
-    
-
-    const char* fragment_source =
-    R"glsl(
-        #version 300 es
-
-        out mediump vec4 outColor;
-
-        void main()
-        {
-            outColor = vec4(1.0, 1.0, 1.0, 1.0);
-        }
-    )glsl";
-    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    {
-        glShaderSource(fragment_shader, 1, &fragment_source, nullptr);
-        glCompileShader(fragment_shader);
-        GLint status;
-        glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &status);
-        if (status != GL_TRUE) {
-            char buffer[512];
-            glGetShaderInfoLog(fragment_shader, 512, NULL, buffer);
-            std::cerr << "Fragment shader error: " << buffer << std::endl;
-        }
-    }
-
-    GLuint shader_program = glCreateProgram();
-    {
-        glAttachShader(shader_program, vertex_shader);
-        glAttachShader(shader_program, fragment_shader);
-    }
-    
-    glLinkProgram(shader_program);
-    glUseProgram(shader_program);
-
-    GLint pos_attrib = glGetAttribLocation(shader_program, "position");
-    glVertexAttribPointer(pos_attrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(pos_attrib);
-
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-
-    while (not glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-            glfwSetWindowShouldClose(window, GL_TRUE);
-        }
+    while(gfx.draw()) {
+        // do game logic here
     }
 
     glfwTerminate();
